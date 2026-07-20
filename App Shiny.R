025b85a18bbb456f8e91f7e8cacf7c717d0609a2
Agustin Baltrons <agusbaltrons@gmail.com>
2026-07-20 03:58:28 -0300

    Terminamos la UI de la shiny

--- App Shiny.R
+++ App Shiny.R
@@ -2,7 +2,23 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("All Star")
+  titlePanel("Predicción de selección al All Star"),
+  p("Ingrese las estadisticas promedio por partido de un jugador. El modelo estimara las probabilidad de ser seleccionado al All Star con esos números:"),
+  sidebarLayout(
+  sidebarPanel(h3("Estadísticas del jugador:"),
+    sliderInput("PTS", "Promedio de puntos:", min = 0, max = 50, value = 0, step = 0.1),
+    sliderInput("AST", "Promedio de asistencias:", min = 0, max = 15, value = 0, step = 0.1),
+    sliderInput("TRB", "Promedio de rebotes:", min = 0, max = 25, value = 0, step = 0.1),
+    sliderInput("STL", "Promedio de robos:", min = 0, max = 5, value = 0, step = 0.1),
+    sliderInput("BLK", "Promedio de tapas:", min = 0, max = 5, value = 0, step = 0.1),
+    actionButton("predecir", 
+                 "Calcular probabilidad")),
+  mainPanel(h2("Probabilidad de ser All-Star"),
+    h1("—"),
+    br(),
+    h2("Clasificación estimada"),
+    h2("—"))
+  )
 )
 
 
