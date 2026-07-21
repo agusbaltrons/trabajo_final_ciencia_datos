48a01ddf455710ad292473bdbbb2084813b22155
mardamiano <marinadamiano27@gmail.com>
2026-07-20 23:52:53 -0300

    shinyyyy

--- App Shiny.R
+++ App Shiny.R
@@ -1,26 +1,59 @@
 #LIBRERIAS
 library(shiny)
 library(tidyverse)
+library(bslib)
 
 nba_rf <- readRDS("nba_rf.rds")
 
-ui <- fluidPage(
-  titlePanel("Predicción de selección al All Star"),
+ui <- page_sidebar(
+  tags$head(
+    tags$style(HTML("
+      .navbar-brand {
+        font-size: 36px;
+        font-weight: bold;
+      }
+    "))
+  ),
+  title = "Predicción de selección al All-Star",
   p("Ingrese las estadisticas promedio por partido de un jugador. El modelo estimara las probabilidad de ser seleccionado al All Star con esos números:"),
-  sidebarLayout(
-  sidebarPanel(h3("Estadísticas del jugador:"),
-    sliderInput("PTS", "Promedio de puntos:", min = 0, max = 50, value = 0, step = 0.1),
-    sliderInput("AST", "Promedio de asistencias:", min = 0, max = 15, value = 0, step = 0.1),
-    sliderInput("TRB", "Promedio de rebotes:", min = 0, max = 25, value = 0, step = 0.1),
-    sliderInput("STL", "Promedio de robos:", min = 0, max = 5, value = 0, step = 0.1),
-    sliderInput("BLK", "Promedio de tapas:", min = 0, max = 5, value = 0, step = 0.1),
-    actionButton("predecir", 
-                 "Calcular probabilidad")),
-  mainPanel(h2("Probabilidad de ser All-Star"),
-    h1(textOutput("probabilidad")),
+  theme = bs_theme(
+    bootswatch = "flatly",
+    primary = "#1D3557"
+  ),
+  sidebar = sidebar(
+    h4("Estadísticas del jugador"),
+    sliderInput("PTS", "Promedio de puntos", min = 0, max = 50, value = 0, step = 0.1),
+    sliderInput("AST", "Promedio de asistencias", min = 0, max = 15, value = 0, step = 0.1),
+    sliderInput("TRB", "Promedio de rebotes", min = 0, max = 25, value = 0, step = 0.1),
+    sliderInput("STL", "Promedio de robos", min = 0, max = 5, value = 0, step = 0.1),
+    sliderInput("BLK", "Promedio de tapas", min = 0, max = 5, value = 0, step = 0.1),
     br(),
-    h2("Clasificación estimada"),
-    h2(textOutput("clasificacion")))
+    actionButton(
+      "predecir",
+      "Calcular probabilidad",
+      class = "btn-primary"
+    )
+  ),
+  layout_column_wrap(
+    width = 1,
+    card(
+      card_header("📈 Probabilidad de ser All-Star"),
+      card_body(
+        div(
+          style = "text-align:center;",
+          h1(textOutput("probabilidad"))
+        )
+      )
+    ),
+    card(
+      card_header("🏆 Clasificación estimada"),
+      card_body(
+        div(
+          style = "text-align:center;",
+          h2(textOutput("clasificacion"))
+        )
+      )
+    )
   )
 )
 
